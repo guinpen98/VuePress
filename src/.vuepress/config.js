5a8f29f7645d70f8fd84cace3874631bfda8b939
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2022-09-01 01:08:58 +0900

    Fixing PATH

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,13 +1,6 @@
 module.exports = {
   title: 'GitHub Pages product by VuePress',
   description: 'VuePress',
-  base: '/vue-press/src/.vuepress/dist',
-  configureWebpack: (config) => {
-    // 1) xserver で確認したい場合
-    config.output.publicPath = 'VuePress が出力したファイルを配置するディレクトリへのパス'
-    
-    // 2) ローカルの環境で確認したい場合
-    const path = require('path')
-    config.output.publicPath = path.resolve(__dirname, 'dist')
-  }
+  dest: 'docs/',
+  base: '.',
 }
