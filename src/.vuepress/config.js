05c1f28024d63f2c36dc2659247531970f7025b3
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2022-09-01 00:52:34 +0900

    base

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,6 +1,13 @@
 module.exports = {
   title: 'GitHub Pages product by VuePress',
-  description: 'VuePress やっていき',
-  dest: 'docs/',
-  base: '',
+  description: 'VuePress',
+  base: 'src/.vuepress/dist',
+  configureWebpack: (config) => {
+    // 1) xserver で確認したい場合
+    config.output.publicPath = 'VuePress が出力したファイルを配置するディレクトリへのパス'
+    
+    // 2) ローカルの環境で確認したい場合
+    const path = require('path')
+    config.output.publicPath = path.resolve(__dirname, 'dist')
   }
+}
