033d6bb27edbb9baa8fe9f2a29ac41cd6cc06a4d
guinpen98 <guinpen9988@gmail.com>
2025-04-17 23:48:11 +0900

    fix: scroll

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -31,6 +31,24 @@ module.exports = {
       },
     ]
   },
+  enhanceAppFiles: {
+    name: 'custom-scroll',
+    content: `
+      export default ({ router }) => {
+        router.options.scrollBehavior = (to, from, savedPosition) => {
+          if (savedPosition) {
+            return savedPosition
+          } else if (to.hash) {
+            return {
+              selector: to.hash
+            }
+          } else {
+            return { x: 0, y: 0 }
+          }
+        }
+      }
+    `
+  },
   markdown: {
     toc: { includeLevel: [2] },
     anchor: {
