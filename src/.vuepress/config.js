817109b90528fdc2f30b851643052813e1c7b960
guinpen98 <guinpen9988@gmail.com>
2025-04-17 23:58:41 +0900

    fix

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -31,28 +31,10 @@ module.exports = {
       },
     ]
   },
-  enhanceAppFiles: {
-    name: 'custom-scroll',
-    content: `
-      export default ({ router }) => {
-        router.options.scrollBehavior = (to, from, savedPosition) => {
-          if (savedPosition) {
-            return savedPosition
-          } else if (to.hash) {
-            return {
-              selector: to.hash
-            }
-          } else {
-            return { x: 0, y: 0 }
-          }
-        }
-      }
-    `
-  },
   markdown: {
     toc: { includeLevel: [2] },
     anchor: {
-      permalinkSymbol: '#'
+      permalink: false
     },
     linkify: true
   },
